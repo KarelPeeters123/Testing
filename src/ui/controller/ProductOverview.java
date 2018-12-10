0b7ef895b618c53897e710d04cc2c66b1a5bed4b
Dieter Dehaes <dieter.dehaes@student.ucll.be>
2018-12-10 20:04:23 +0100

    Update ProductOverview.java

--- src/ui/controller/ProductOverview.java
+++ src/ui/controller/ProductOverview.java
@@ -1,7 +1,10 @@
 package ui.controller;
 
+import domain.db.Cart;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ProductOverview extends RequestHandler {
 
@@ -12,6 +15,17 @@ public class ProductOverview extends RequestHandler {
 
     public String handleProductOverviewRequest(HttpServletRequest request, HttpServletResponse response) {
         request.setAttribute("products", getService().getProducts());
+
+        Cart cart;
+        HttpSession session = request.getSession();
+        if (session.getAttribute("cart") == null) {
+            cart = new Cart(getService());
+        } else {
+            cart = (Cart) session.getAttribute("cart");
+        }
+
+        request.setAttribute("total", cart.totalAmountOfProducts());
+
         return "productoverview.jsp";
     }
 
